1a3d2420fa1b3672369765f2c898db526aa69f82
unknown <jaylesh27@gmail.com>
2017-09-22 16:12:20 -0400

    added content to about me and projects sections

---
+++ public/assets/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$("div a.page-scroll").on("click", function(e){
+		e.preventDefault();
+		$("html body").animate({ scrollTop: $("#about-me").offset().top }, 1000);
+	});
+
+	
+
+});
